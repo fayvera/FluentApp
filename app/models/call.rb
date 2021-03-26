8ed353b2856045df536d4f9df2b92bd2eb8c23fc
Fay Vera <fvera.cavalcante@gmail.com>
2021-03-25 22:57:23 -0400

    establishes relationships user-call

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,2 +1,4 @@
 class Call < ApplicationRecord
+    belongs_to :speaker, class_name: "User"
+    belongs_to :caller, class_name: "User"    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates_presence_of :name, :username, :email, :password 
+
+    has_many :calls
+
+    #as a caller
+    has_many :speakers, foreign_key: speaker_id, class_name: "Call"
+
+    #as a speaker
+    has_many :callers, foreign_key: caller_id, class_name: "Call"
+
+
+    private
 end
