4577018ad44e84a56f284a747722447c7681af5c
Fay Vera <fvera.cavalcante@gmail.com>
2021-03-26 11:29:06 -0400

    adds admin column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
     has_secure_password
     validates_presence_of :name, :username, :email, :password 
 
-    has_many :calls
 
     #as a caller
-    has_many :speakers, foreign_key: speaker_id, class_name: "Call"
+    has_many :speakers_calls, foreign_key: speaker_id, class_name: "Call"
+    has_many :speakers, through: :speakers_calls, class_name: "User"
 
     #as a speaker
-    has_many :callers, foreign_key: caller_id, class_name: "Call"
+    has_many :callers_calls, foreign_key: caller_id, class_name: "Call"
+    has_many :callers, through: :callers_calls, class_name: "User"
 
 
     private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_030304) do
+ActiveRecord::Schema.define(version: 2021_03_26_152742) do
 
   create_table "calls", force: :cascade do |t|
     t.time "schedule_time"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_030304) do
     t.string "native_language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
