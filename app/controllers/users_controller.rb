aba4f396fb1ee6042c006f3435e5057e38ca730a
Fay Vera <fvera.cavalcante@gmail.com>
2021-03-25 23:43:28 -0400

    adds actions to users and error messages to form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+    def new 
+        @user = User.new
+    end
 
-    
+    def create
+        if user = User.create(user_params)
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            render 'new'
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :username,
+            :email,
+            :password
+            :native_language
+        )
+    end
 end
